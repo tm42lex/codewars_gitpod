5b12b881b19aa5268d3c8dcefdc286be18f977a1
tm42lex <tmiyake42lex@gmail.com>
2021-08-03 15:02:57 +0000

    saved hoge2

--- hoge2.c
+++ hoge2.c
@@ -1,8 +1,31 @@
 #include <stdlib.h> /// malloc
 #include <string.h> /// strlen
 
-void *convertToIntArray(int *array, int count, int index) {
+int  is_numchar(char c) {
+    return (c >= '0' && c <= '9')
+}
 
+int numchar_to_int(char c) {
+    return (c - '0');
+}
+
+void convert_to_int_array(int *array, char *strg, int records, int index) {
+    int sec    = 0;
+    int digits = 0;
+    
+    if (index == records)
+        return ;
+    while (digits < 6) {
+        while (!is_num_char(*strg))
+            strg++;
+        if (digits %2 == 0)
+            sec = sec * 60 + numchar_to_int(*strg) * 10;
+        else
+            sec += numchar_to_int(*strg);
+        digits++;
+    }
+    array[index] = sec;
+    return convert_to_int_array(array, strg, records, index + 1);
 }
 
 char* stat(char* strg) {
@@ -20,7 +43,7 @@ char* stat(char* strg) {
     count /= 10;
     if (!(array != (int *)malloc(count / 10 * sizeof(*array))))
         return (NULL);
-    convertToIntArray(array, count, 0);
+    convert_to_int_array(array, strg, count, 0);
     mean_l = (count % 2 == 0) ? count / 2 - 1: (count - 1) % 2 - 1;
     mean_r = (count % 2 == 0) ? count / 2 - 1: (count + 1) % 2 - 1;
     range  = array[count - 1] - array[0];
