e2b8338efc519efa911a275ed705d1e85cf3ed8d
tm42lex <tmiyake42lex@gmail.com>
2021-08-11 17:15:44 +0000

    added note on merge sort

--- mergesort.c
+++ mergesort.c
@@ -4,15 +4,14 @@ void merge(int *array, int h_index, int m_index, int l_index) {
     
 }
 
-void merge_sort(int *array, int h_index, int m_index, int l_index) {
-    // printf("%d, %d, %d,\n", h_index, m_index, l_index);
-    int nm_index = (h_index + l_index) / 2;
-    if (nm_index != h_index && nm_index != l_index) {
-        merge_sort(array, h_index, nm_index, m_index);
-        merge_sort(array, m_index, nm_index, l_index);
+void merge_sort(int *array, int h_index, int l_index) {
+    printf("%d, %d, %d,\n", h_index, m_index, l_index);
+    if (l_index < h_index) {
+        int m_index = l_index + (h_index - l_index) / 2; //最低値プラス差分の重複にすることで、int型のオーバーフローがあった時用の対策
+        merge_sort(array, h_index, m_index + 1); // + 1 は重複避けるため
+        merge_sort(array, m_index, l_index);
+        merge(array, h_index, m_index, l_index);
     }
-    if (nm_index == m_index) printf("%d\n", nm_index);
-    merge(array, h_index, m_index, l_index);
 }
 
 int main() {
@@ -23,9 +22,8 @@ int main() {
 
     h_index = sizeof(hoge)/sizeof(*hoge) - 1;
     l_index = 0;
-    m_index = 0;
 
-    merge_sort(hoge, h_index, m_index, l_index);
+    merge_sort(hoge, h_index, l_index);
     for (int i = 0; i < sizeof(hoge)/sizeof(*hoge); i++) {
         // printf("%d\n", hoge[i]);
     }
