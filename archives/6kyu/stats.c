9e7262eb28932327223187cc536f12b8d3b8ad20
tm42lex <tmiyake42lex@gmail.com>
2021-08-28 15:37:53 +0000

    finally wrappeed up stats.c

--- archives/6kyu/stats.c
+++ archives/6kyu/stats.c
@@ -1,9 +1,11 @@
+/// https://www.codewars.com/kata/55b3425df71c1201a800009c/
+
 #include <stdlib.h> /// malloc, free
 #include <string.h> /// strlen
 
-#include <stdio.h>
+#include <stdio.h> /// sprintf
 
-void merge(int *array, int l_index, int m_index, int r_index) {
+int merge(int *array, int l_index, int m_index, int r_index) {
     int *temp_r;
     int *temp_l;
     int r_len;
@@ -13,11 +15,9 @@ void merge(int *array, int l_index, int m_index, int r_index) {
     l_len = m_index + 1 - l_index;
 
     if (!(temp_r = malloc(r_len * sizeof(*temp_r))))
-        // return (NULL);
-        return ;
+        return (-1);
     if (!(temp_l = malloc(l_len * sizeof(*temp_l))))
-        // return (NULL);
-        return ;
+        return (-1);
     
     int r = 0 - 1;
     while(++r < r_len) 
@@ -43,15 +43,18 @@ void merge(int *array, int l_index, int m_index, int r_index) {
         array[l_index++] = temp_l[l++];
     free(temp_l);
     free(temp_r);
+    return (0);
 }
 
-void merge_sort(int *array, int l_index, int r_index) {
+int merge_sort(int *array, int l_index, int r_index) {
     if (l_index < r_index) {
         int m_index = l_index + (r_index - l_index) / 2;
         merge_sort(array, l_index, m_index);
         merge_sort(array, m_index + 1, r_index);
-        merge(array, l_index, m_index, r_index);
+        if (merge(array, l_index, m_index, r_index) == -1)
+            return (-1);
     }
+    return (0);
 }
 
 char num_padleft_zero(int num, int is_last_digit) {
@@ -61,13 +64,10 @@ char num_padleft_zero(int num, int is_last_digit) {
     return ('0' + base % 10);
 }
 
-char *sec_to_HMS(int s) {
+void sec_to_HMS(int s, char *ret) {
     int h;
     int m;
-    char *ret;
 
-    if (!(ret = malloc(6 + 2 + 1)))
-        return (NULL);
     h = s / (60 * 60);
     s -= 60 * 60 * h ;
     m = s / 60;
@@ -89,7 +89,6 @@ char *sec_to_HMS(int s) {
         d++;
     }
     ret[6 + 2] = '\0';
-    return ret;
 }
 
 int  is_numchar(char c) {
@@ -100,12 +99,12 @@ int numchar_to_int(char c) {
     return (c - '0');
 }
 
-void convert_to_int_array(int *array, char *strg, int *sum, int records, int index) {
+int convert_to_int_array(int *array, char *strg, int *sum, int records, int index) {
     int sec    = 0;
     int digits = 0;
     
     if (index == records)
-        return ;
+        return (0);
     while (digits < 6 && *strg) {
         while (*strg && !is_numchar(*strg))
             strg++;
@@ -118,7 +117,8 @@ void convert_to_int_array(int *array, char *strg, int *sum, int records, int ind
     }
     *sum += sec;
     array[index] = sec;
-    return convert_to_int_array(array, strg, sum, records, index + 1);
+    if (convert_to_int_array(array, strg, sum, records, index + 1) == -1) 
+        return (-1);
 }
 
 char* stat(char* strg) {
@@ -131,6 +131,9 @@ char* stat(char* strg) {
     int median_l;
     int median_r;
     char *ret;
+    char *average_str;
+    char *median_str;
+    char *range_str;
     
     count = strlen(strg) + 2; // for missing ", " at the end
     if (count < 2 || count %10 != 0) 
@@ -140,9 +143,19 @@ char* stat(char* strg) {
         return (NULL);
     if (!(ret = (char *)malloc(50 + 1)))
         return (NULL);
+    if (!(average_str = malloc(6 + 2 + 1)))
+        return (NULL);
+    if (!(range_str = malloc(6 + 2 + 1)))
+        return (NULL);
+    if (!(median_str = malloc(6 + 2 + 1)))
+        return (NULL);
+    
     average = 0;
-    convert_to_int_array(array, strg, &average, count, 0);
-    merge_sort(array, 0, count - 1);
+    if (convert_to_int_array(array, strg, &average, count, 0) == -1) 
+        return (NULL);
+    if (merge_sort(array, 0, count - 1) == -1)
+        return (NULL);
+  
 
     average  /= count;
     median_l  = (count % 2 != 0) ? count / 2 : (count - 1) / 2;
@@ -150,13 +163,20 @@ char* stat(char* strg) {
     median    = (array[median_r] + array[median_l]) / 2;
     range     = array[count - 1] - array[0];
     
-    printf("%s\n%s\n%s\n", sec_to_HMS(range), sec_to_HMS(average), sec_to_HMS(median));
-    free(array);
-    return (NULL);
+    sec_to_HMS(range, range_str);
+    sec_to_HMS(average, average_str);
+    sec_to_HMS(median, median_str);
+    sprintf(ret, "Range: %s Average: %s Median: %s", range_str, average_str, median_str);
+    ret[50 + 1] = '\n';
+    free(range_str);
+    free(average_str);
+    free(median_str);
+    return (ret);
 }
 
 int main() {
     char* l = "02|15|59, 02|47|16, 02|17|20, 02|32|34, 02|17|17, 02|22|00, 02|31|41";
-    stat(l);
+    char *hoge = stat(l);
+    printf("%s\n", hoge);
     return (0);
 }
